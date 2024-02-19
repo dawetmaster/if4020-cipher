2b03efaca5589d6cc77756ae7a3024d40ffed1e8
Alifia Rahmah <alifiarahmah03@gmail.com>
2024-02-19 16:53:11 +0700

    feat: implement standard vigenere

--- be/routers/vigenere.py
+++ be/routers/vigenere.py
@@ -12,3 +12,45 @@ router = APIRouter(
 @router.get("/")
 async def vigenere():
     return "Vigenere Cipher API"
+
+@router.post("/encrypt")
+async def vigenere_encrypt(plaintext: str, key: str):
+    # make plaintext and key lowercase
+    plaintext = plaintext.lower()
+    key = key.lower()
+
+    # remove non-alphabet characters
+    plaintext = "".join(filter(str.isalpha, plaintext))
+
+    # repeat key
+    key = key * (len(plaintext) // len(key)) + key[:len(plaintext) % len(key)]
+
+    ciphertext = ""
+    for i in range(len(plaintext)):
+        ciphertext += (chr((ord(plaintext[i]) + ord(key[i]) - 2 * ord('a')) % 26 + ord('a'))).upper()
+
+    return {
+        "status": "success",
+        "ciphertext": ciphertext
+    }
+
+@router.post("/decrypt")
+async def vigenere_decrypt(ciphertext: str, key: str):
+    # make ciphertext and key lowercase
+    ciphertext = ciphertext.lower()
+    key = key.lower()
+
+    # remove non-alphabet characters
+    plaintext = "".join(filter(str.isalpha, ciphertext))
+
+    # repeat key
+    key = key * (len(ciphertext) // len(key)) + key[:len(ciphertext) % len(key)]
+
+    plaintext = ""
+    for i in range(len(ciphertext)):
+        plaintext += (chr((ord(ciphertext[i]) - ord(key[i]) + 26) % 26 + ord('a'))).upper()
+
+    return {
+        "status": "success",
+        "plaintext": plaintext
+    }
