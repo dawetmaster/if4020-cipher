1fd87556f8c4277b46ab4a8b47fe108e9f4b857c
Alifia Rahmah <alifiarahmah03@gmail.com>
2024-02-20 14:30:23 +0700

    feat: add affine cipher

--- be/routers/affine.py
+++ be/routers/affine.py
@@ -11,4 +11,51 @@ router = APIRouter(
 
 @router.get("/")
 async def affine():
-    return "Affine Vigenere Cipher API"
+    return "Affine Cipher API"
+
+@router.post("/encrypt")
+async def affine_encrypt(plaintext: str, m: int, b: int):
+    # make plaintext lowercase
+    plaintext = plaintext.lower()
+
+    # remove non-alphabet characters
+    plaintext = "".join(filter(str.isalpha, plaintext))
+
+    ciphertext = ""
+    for i in range(len(plaintext)):
+        ciphertext += chr((m * (ord(plaintext[i]) - ord('a')) + b) % 26 + ord('a'))
+
+        chr(m * (ord('k') - ord('a')))
+
+    return {
+        "status": "success",
+        "ciphertext": ciphertext
+    }
+
+@router.post("/decrypt")
+async def affine_decrypt(ciphertext: str, m: int, b: int):
+    # make ciphertext lowercase
+    ciphertext = ciphertext.lower()
+
+    # remove non-alphabet characters
+    ciphertext = "".join(filter(str.isalpha, ciphertext))
+
+    # find modular multiplicative inverse of m
+    i = 0
+    while (m * i) % 26 != 1:
+        i += 1
+        if i > 26:
+            return {
+                "status": "error",
+                "message": "Invalid m"
+            }
+    m = i
+
+    plaintext = ""
+    for i in range(len(ciphertext)):
+        plaintext += chr((m * (ord(ciphertext[i]) - ord('a') - b)) % 26 + ord('a'))
+
+    return {
+        "status": "success",
+        "plaintext": plaintext
+    }
