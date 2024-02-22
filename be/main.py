471d877e1fcf873cf2b78d7a122f38a4ab40345e
Alifia Rahmah <alifiarahmah03@gmail.com>
2024-02-22 10:00:37 +0700

    feat: add jinja templating

--- be/main.py
+++ be/main.py
@@ -1,14 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 from be.routers import affine, extendedVigenere, super, vigenere
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="be/templates")
 
 app.include_router(affine.router)
 app.include_router(extendedVigenere.router)
 app.include_router(super.router)
 app.include_router(vigenere.router)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+  return templates.TemplateResponse("index.html", {"request": request})
