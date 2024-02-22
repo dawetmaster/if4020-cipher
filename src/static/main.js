5e5433d6201a0eb466e75e391e8648f24e6189a6
Alifia Rahmah <alifiarahmah03@gmail.com>
2024-02-22 14:59:30 +0700

    feat: add toggle base64

--- src/static/main.js
+++ src/static/main.js
@@ -5,86 +5,93 @@ let inputtype = 'text';
 let submitURL = ''
 const form = document.getElementById('form');
 const resultDiv = document.getElementById('result');
+const base64Toggle = document.getElementById('base64toggle');
 
 window.onload = () => {
-    console.log("OK");
-}
+  console.log('OK');
+};
 
 function setRequestToJSON() {
-    // Get form data
-    const formData = new FormData(form);
+  // Get form data
+  const formData = new FormData(form);
 
-    // Convert form data to JSON
-    const jsonData = {};
-    formData.forEach((value, key) => {
-        jsonData[key] = value;
-    });
+  // Convert form data to JSON
+  const jsonData = {};
+  formData.forEach((value, key) => {
+    jsonData[key] = value;
+  });
 
-    return jsonData;
+  return jsonData;
 }
 
 function setURL(algorithm, enctype, inputtype) {
-    let url = ''
-    if (enctype === 'encrypt' && inputtype === 'text') {
-        url = `/${algorithm}/encrypt`
-    }
-    else if (enctype === 'encrypt' && inputtype === 'file') {
-        url = `/${algorithm}/encryptfile`
-    }
-    else if (enctype === 'decrypt' && inputtype === 'text') {
-        url = `/${algorithm}/decrypt`
-    }
-    else if (enctype === 'decrypt' && inputtype === 'file') {
-        url = `/${algorithm}/decryptfile`
-    }
-    document.getElementById("form").action = url;
-    submitURL = url
-    console.log(url);
+  let url = '';
+  if (enctype === 'encrypt' && inputtype === 'text') {
+    url = `/${algorithm}/encrypt`;
+  } else if (enctype === 'encrypt' && inputtype === 'file') {
+    url = `/${algorithm}/encryptfile`;
+  } else if (enctype === 'decrypt' && inputtype === 'text') {
+    url = `/${algorithm}/decrypt`;
+  } else if (enctype === 'decrypt' && inputtype === 'file') {
+    url = `/${algorithm}/decryptfile`;
+  }
+  document.getElementById('form').action = url;
+  submitURL = url;
+  console.log(url);
 }
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let reqBody = setRequestToJSON();
-    console.log(submitURL);
-    console.log(JSON.stringify(reqBody));
-    fetch(`${submitURL}`, {
-        method: "POST",
-        headers: {
-            applicationType: "application/json",
-        },
-        body: JSON.stringify(reqBody)
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let reqBody = setRequestToJSON();
+  console.log(submitURL);
+  console.log(JSON.stringify(reqBody));
+  fetch(`${submitURL}`, {
+    method: 'POST',
+    headers: {
+      applicationType: 'application/json',
+    },
+    body: JSON.stringify(reqBody),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // Display response in the result div
+      resultDiv.innerHTML = `${JSON.stringify(data, null, 2)}`;
     })
-    .then(response => response.json())
-    .then(data => {
-        // Display response in the result div
-        resultDiv.innerHTML = `${JSON.stringify(data, null, 2)}`;
-    })
-    .catch(error => {
-        console.error('Error:', error);
+    .catch((error) => {
+      console.error('Error:', error);
     });
-})
+});
 
-document.getElementById("encryptselect").addEventListener("click", () => {
-    enctype = "encrypt";
-    setURL(algorithm, enctype, inputtype);
-})
+document.getElementById('encryptselect').addEventListener('click', () => {
+  enctype = 'encrypt';
+  setURL(algorithm, enctype, inputtype);
+});
 
-document.getElementById("decryptselect").addEventListener("click", () => {
-    enctype = "decrypt";
-    setURL(algorithm, enctype, inputtype);
-})
+document.getElementById('decryptselect').addEventListener('click', () => {
+  enctype = 'decrypt';
+  setURL(algorithm, enctype, inputtype);
+});
 
-document.getElementById("textselect").addEventListener("click", () => {
-    inputtype = "text";
-    setURL(algorithm, enctype, inputtype);
-})
+document.getElementById('textselect').addEventListener('click', () => {
+  inputtype = 'text';
+  setURL(algorithm, enctype, inputtype);
+});
 
-document.getElementById("fileselect").addEventListener("click", () => {
-    inputtype = "file";
-    setURL(algorithm, enctype, inputtype);
-})
+document.getElementById('fileselect').addEventListener('click', () => {
+  inputtype = 'file';
+  setURL(algorithm, enctype, inputtype);
+});
 
-document.getElementById("algoselect").addEventListener("change", () => {
-    algorithm = document.getElementById("algoselect").value;
-    setURL(algorithm, enctype, inputtype);
-})
+document.getElementById('algoselect').addEventListener('change', () => {
+  algorithm = document.getElementById('algoselect').value;
+  setURL(algorithm, enctype, inputtype);
+});
+
+base64Toggle.addEventListener('click', () => {
+  // change input in resultDiv to
+  if (base64Toggle.checked) {
+    resultDiv.value = btoa(resultDiv.value);
+  } else {
+    resultDiv.value = atob(resultDiv.value);
+  }
+});
